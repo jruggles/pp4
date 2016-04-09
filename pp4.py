76ba2acd6b44622c882c62f29da8152066b57705
Jonathan Ruggles <ruggles@newmedio.com>
2016-04-08 19:10:35 -0500

    stopping for the day

--- pp4.py
+++ pp4.py
@@ -10,28 +10,29 @@ import sys
 import datetime
 
 
-def optRoot():
+def optRoot(x):
 	e = sys.float_info.epsilon
-	y = 45285668.165322
+	y = x
 	a, exp = frexp(y)
 	nfraction = 1.0/3.0
-
+	iteration = 0
 	shiftexp = exp%3
 	if shiftexp > 0:
 		shiftexp -= 3
 	nexp = (exp - shiftexp) / 3
 	a = ldexp(a, shiftexp)
-	print nfraction
+	# print nfraction
 	approx = nfraction * a
 	better = ((nfraction)*(((2.0)*approx)+((a)/(approx*approx))))
-	print better - approx
+	# print better - approx
 
 	while abs(better - approx) > e:
-		print approx
+		# print approx
 		approx = better
 		better = ((nfraction)*(((2.0)*approx)+((a)/(approx*approx))))
-		print "test: ", better - approx
-		print e
+		# print "test: ", better - approx
+		# print e
+		iteration += 1
 
 	y = ldexp(approx, nexp)
 	return y
@@ -63,14 +64,16 @@ if __name__ == "__main__":
 '''
 Initialize the input and output data
 '''
-#A = np.matrix(np.loadtxt(inA))
+M = np.matrix(np.loadtxt(inA))
+A = np.squeeze(np.asarray(M))
+print A
 
 #X = np.random.uniform(1000000, 100000000, 10000)
 
 print datetime.datetime.now()
 
-cubeA = optRoot()
-print cubeA
+for x in A:
+	cubeA = optRoot(x)
 
 print datetime.datetime.now()
 
